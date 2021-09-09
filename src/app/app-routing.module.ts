082314c3f9f43a6f7f688f7fda2b9b4b4b1d6563
LashchA <lashch.andriy25@gmail.com>
2021-09-09 14:23:04 +0300

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,27 @@
+import { HobbiesComponent } from './hobbies/hobbies.component';
+import { PortfolioComponent } from './portfolio/portfolio.component';
+import { AboutComponentComponent } from './about-component/about-component.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-
-const routes: Routes = [];
+import { HomeComponent } from './home/home.component';
+const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent
+  },
+  {
+    path: 'about',
+    component: AboutComponentComponent
+  },
+  {
+    path: 'portfolio',
+    component: PortfolioComponent
+  },
+  {
+    path: 'hobbies',
+    component: HobbiesComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
